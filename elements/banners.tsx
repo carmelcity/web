75eafb7e61f6734c67e810c6fb9d22ab9c2bd4c6
Chunky <chunky@fluidtrends.com>
2025-01-05 02:31:06 +0100

    updated profile cards

--- elements/banners.tsx
+++ elements/banners.tsx
@@ -6,9 +6,9 @@ export const BannerImage = ({ banner, isEditable, onEdit, forceBanner, thumbnail
   return <div
       className={`w-full flex-col flex justify-center items-center`}>
       <div className="lg:hidden block relative flex">
-            <Image src={forceBanner ? banner : thumbnail} alt="card" className="object-cover w-full" width={300} height={300}/>
+            <Image src={forceBanner ? banner : thumbnail} alt="card" className="object-cover w-full" width={500} height={500}/>
       </div>
-      <div className="hidden lg:block relative flex bg-black border-4">
+      <div className="hidden lg:block relative flex bg-black w-full">
             <Image src={banner} alt="card" className="object-cover w-full" width={1500} height={500}/>
       </div>  
       { isEditable && <div className={`${readex_pro.className} flex text-white bg-black bg-opacity-40 lg:p-4 p-1 mr-2 font-thin cursor-pointer absolute items-center mt-2 top-0 right-0 border border-1 border-cyan`} onClick={onEdit}>
