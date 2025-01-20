cc8e9494752314b7f0ac7308b1496c1137ef7060
Chunky <chunky@fluidtrends.com>
2025-01-20 18:10:42 +0100

    tweaked carmel uis

--- screens/ProjectsListScreen.tsx
+++ screens/ProjectsListScreen.tsx
@@ -11,10 +11,15 @@ export const ProjectsListScreen = () => {
       subtitle="Projects"
       icon="SparklesIcon"
       name="accounts"
+      wide
       filter={(item: any) => item.type === "project"}   
       placeholder={ListPlaceholder}
       shortIntro
       card={ProjectCard}
-    />
+    >
+       <span className='font-normal text-transparent bg-clip-text bg-gradient-to-r from-cyan to-light-green text-md mb-8 text-center'>
+                Carmel Projects are innovation initiatives owned, funded and developed by communities.
+        </span>
+    </ListScreen>
   }
   
