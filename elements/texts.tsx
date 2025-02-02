0fb25398257715f4fa8150f03b51a2c615a2dfbd
Chunky <chunky@fluidtrends.com>
2025-02-02 22:09:17 +0100

    fixed show more section for comments

--- elements/texts.tsx
+++ elements/texts.tsx
@@ -15,19 +15,29 @@ export const PostText = ({ text }: any) => {
         <p className={show ? '' : `line-clamp-2`}>
             { text }
         </p>
-        { text.length > SM_MAX && <button className='text-sm mt-4 text-gray-500 flex flex-row lg:hidden' onClick={onToggle}>        
+        <div className='flex flex-row w-full justify-start'>
+        { text.length > SM_MAX && <button className='text-sm text-gray-500 flex flex-row lg:hidden' onClick={onToggle}>        
              {show ? `show less` : `show more`}
             <DynamicIcon name={show ? 'ChevronUpIcon' : 'ChevronDownIcon'} width={16} height={16} className='ml-1 mt-0.5 text-white'/>
         </button> }
-        { text.length > LG_MAX && <button className='text-sm mt-4 text-gray-500 invisible lg:visible flex' onClick={onToggle}>        
+        { text.length > LG_MAX && <button className='text-sm text-gray-500 hidden lg:flex flex flex-row' onClick={onToggle}>        
              {show ? `show less` : `show more`}
             <DynamicIcon name={show ? 'ChevronUpIcon' : 'ChevronDownIcon'} width={16} height={16} className='ml-1 mt-0.5 text-white'/>
         </button> }
+        </div>
     </span>
 }
 
 export const PostIntro = ({ text, short }: any) => { 
-   return  <p className={`${readexPro.className} whitespace-break-spaces text-md font-thin text-gray-400 ${short ? 'line-clamp-2' : ''}`}>
+    const [show, setShow] = useState(false)
+
+    const onToggle = () => {
+      setShow(!show)
+    }
+
+   return  <span className='whitespace-pre text-gray-300 text-wrap'>
+    <p className={`${readexPro.className} whitespace-break-spaces text-md font-thin text-gray-400 ${short ? 'line-clamp-2' : ''}`}>
         { text }
-    </p>  
+    </p> 
+    </span> 
 }
